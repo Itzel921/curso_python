589ead76c0ea5dee7ae6f6892b62564b7e47cd31
Itzel921 <monroyalvarezitzelalejandra@gmail.com>
2025-01-27 15:31:26 -0600

    Creé el archivo de funciones auxiliares para linea

--- linea/funciones.py
+++ linea/funciones.py
@@ -4,11 +4,17 @@ Funciones auxiliares para el programa "Linea"
 '''
 
 def calcular_y (x,m,b):
-    '''
-        Calcula "y" de acuerdo a la pendiente "m" y el punto de intersección en y "b". Retorna el valor de "y"
-    '''
-    
+    ''' Calcula "y" de acuerdo a la pendiente "m" y el punto de intersección en y "b". Retorna el valor de "y" '''
     return m*x+b
 
 if __name__ == "__main__":
+    x = 0
+    m = 3
+    b = 2
     
+    y = calcular_y(x,m,b)
+    
+    if y == 2:
+        print("Prueba exitosa")
+    else:
+        print("Prueba fallida")
