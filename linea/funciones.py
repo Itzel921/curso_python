5ff638dfc306b0bec34e8b60f6d9b4e66bdf66ae
Itzel921 <monroyalvarezitzelalejandra@gmail.com>
2025-01-27 15:20:37 -0600

    Agregué una nueva carpeta

---
+++ linea/funciones.py
@@ -0,0 +1,14 @@
+'''
+Funciones auxiliares para el programa "Linea"
+
+'''
+
+def calcular_y (x,m,b):
+    '''
+        Calcula "y" de acuerdo a la pendiente "m" y el punto de intersección en y "b". Retorna el valor de "y"
+    '''
+    
+    return m*x+b
+
+if __name__ == "__main__":
+    
