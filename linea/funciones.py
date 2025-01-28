25f622fc99e714b1f13c9b1b50759e0ff3ff41ed
Itzel921 <monroyalvarezitzelalejandra@gmail.com>
2025-01-28 15:39:24 -0600

    Agregué argumentos de linea de comando a app.py

--- linea/funciones.py
+++ linea/funciones.py
@@ -5,16 +5,16 @@ Funciones auxiliares para el programa "Linea"
 
 def calcular_y (x,m,b):
     ''' Calcula "y" de acuerdo a la pendiente "m" y el punto de intersección en y "b". Retorna el valor de "y" '''
-    return m*x+b
+    return (m*x)+b
 
-if __name__ == "__main__":
-    x = 0
-    m = 3
-    b = 2
-    
-    y = calcular_y(x,m,b)
-    
-    if y == 2:
+def test_linea():
+    y = calcular_y(0.0, 2.0, 3.0)
+    return y
+
+
+if __name__ == "__main__":  
+    if test_linea() == 3.0:
         print("Prueba exitosa")
     else:
         print("Prueba fallida")
+        
