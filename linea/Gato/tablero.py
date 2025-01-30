9eaee9daf0d3b01548f2ac065a562d74fbe8aac2
Itzel921 <monroyalvarezitzelalejandra@gmail.com>
2025-01-30 15:37:25 -0600

    Agregue la primera respuesta como usuario y la ia

---
+++ linea/Gato/tablero.py
@@ -0,0 +1,51 @@
+import random
+
+def dibuja_tablero(simbolos:dict):
+    '''
+    Dibuja tablero del juego de el gato
+    '''
+    print(f'''
+    {simbolos['1']} | {simbolos['2']} | {simbolos['3']}
+    ---------
+    {simbolos['4']} | {simbolos['5']} | {simbolos['6']}
+    ---------
+    {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
+    ''')
+    
+def ia(simbolos:dict):
+    ''' Estrategia de la computadora'''
+    ocupado = True
+    while ocupado is True:
+        x = random.choice(list(simbolos.keys()))
+        if simbolos[x] not in ['X','O']:
+            simbolos[x] = 'O'
+            ocupado = False
+        
+    
+def usuario(simbolos:dict):
+    '''Estrategia de usuario'''
+    
+    ocupado = True
+    
+    lista_numeros = [str(i) for i in range(1,10)] #del 1 al 9
+    
+    while ocupado is True:
+        x = input('Elija un número del 1 al 9: ')
+        if x in lista_numeros:
+            if simbolos[x] not in ['X', 'O']:
+                simbolos[x] = 'X'
+                ocupado = False
+        
+        else:
+            print("Esa casilla ya está ocupada")
+            
+                
+    
+if __name__ == '__main__':
+        numeros = [str(i) for i in range(1,10)]
+        dsimbolos = {x:x for x in numeros}
+        dibuja_tablero(dsimbolos)
+        ia(dsimbolos)
+        dibuja_tablero(dsimbolos)
+        usuario(dsimbolos)
+        dibuja_tablero(dsimbolos)
