7c2f16e4ebea033fc99062a9acd1c8f972741ac3
Itzel921 <monroyalvarezitzelalejandra@gmail.com>
2025-01-30 15:37:35 -0600

    Terminado

--- linea/funciones.py
+++ linea/funciones.py
@@ -2,14 +2,26 @@
 Funciones auxiliares para el programa "Linea"
 
 '''
+import matplotlib.pyplot as plt
 
 def calcular_y (x,m,b):
     ''' Calcula "y" de acuerdo a la pendiente "m" y el punto de intersección en y "b". Retorna el valor de "y" '''
     return (m*x)+b
 
+def grafica_linea(X: list, Y:list, m:float, b:float):
+    plt.plot(X,Y)
+    plt.title(f"Línea recta con pendiente={m}")
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid()
+    plt.show()
+    
+    
 def test_linea():
     y = calcular_y(0.0, 2.0, 3.0)
     return y
+
+
 
 
 if __name__ == "__main__":  
