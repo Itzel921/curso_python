d1f98d6aa2bdd37e3a512be9c1094b402d0baceb
Itzel921 <monroyalvarezitzelalejandra@gmail.com>
2025-02-06 15:46:34 -0600

    Despliega score

--- linea/Gato/app.py
+++ linea/Gato/app.py
@@ -4,15 +4,20 @@ import tablero
 
 def main():
     """Función principal"""
+    X = {"G":0, "P":0, "E":0}
+    O = {"G":0, "P":0, "E":0}
+    score = {"X":X, "O":O}
     
+     
     numeros = [str(i) for i in range(1, 10)]
-    dsimbolos = {x:x for x in numeros}
-    g = tablero.juego(dsimbolos)
     
-    if g is not None:
-        print(f"El ganador es {g}")
-    else:
-        print("Empate")
+    corriendo = True
+    while corriendo:
+        dsimbolos = {x:x for x in numeros}
+        g = tablero.juego(dsimbolos) #Trae el ganador
+        tablero.actualizaScore(score,g) #Actualiza el score
+        tablero.despliega_tablero(score) #Despliega el score
+       
 
 if __name__ == '__main__':
     main()
